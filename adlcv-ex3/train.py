97e2cdb996c44cba4e6e24e3802e7f6db53aacc3
Markus <markus.bao.quan@gmail.com>
2025-03-04 15:09:16 +0100

    w3 training loop

--- adlcv-ex3/train.py
+++ adlcv-ex3/train.py
@@ -165,7 +165,7 @@ def main(
     model = model.to(device)
 
     if START_FROM_PRETRAINED_GPT2_CHECKPOINT:
-        model.load_state_dict(torch.load("gpt2_pretrained.pt"))
+        model.load_state_dict(torch.load("/zhome/8a/9/155399/Programmering/02501-ADLCV/adlcv-ex3/gpt2_pretrained.pt"))
 
     # Define the loss function for language modeling.
     # We ignore the padding token (which is now set to eos_token if needed)
